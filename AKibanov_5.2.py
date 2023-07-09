4b65ab4455d71d55351dec3594c078613fdcaa6a
4Kibanov <121761148+4Kibanov@users.noreply.github.com>
2023-07-09 13:16:34 +0500

    №5
    Урок 5

---
+++ AKibanov_5.2.py
@@ -0,0 +1,15 @@
+﻿x = input('Введите английское слово маленькими буквами: ')
+vowels = ['a', 'e', 'i', 'o', 'u']
+a = 0
+b = 0
+print('Количество для каждой гласной буквы:')
+for str in vowels:
+    if str in x:
+        count = x.count(str)
+        print(f'{str}: {count}')
+        a += count
+    else: 
+        print(f'{str}: False')
+b = len(x) - a
+print(f'Количество согласных букв: {b}\n' f'Количество гласных букв: {a}.')
+input('Нажмите Enter для выхода из программы.')
