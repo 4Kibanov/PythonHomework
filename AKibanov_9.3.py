48f27316462544f64e609566ab22b68e8b6a2c23
4Kibanov <121761148+4Kibanov@users.noreply.github.com>
2023-07-26 23:23:49 +0500

    №9

---
+++ AKibanov_9.3.py
@@ -0,0 +1,10 @@
+﻿print('Введите последовательность чисел через пробел, чтобы узнать повторяются числа или нет: ')
+a = list(map(int, input().split()))
+set1 = set()
+for x in (a):
+    if x in set1:
+        print(f'{x}: YES')
+    else:
+        set1.add(x)
+        print(f'{x}: NO')
+input('Нажмите Enter для выхода из программы.')
