4aaea77c383e3e382f978e95ce60df732816771e
4Kibanov <121761148+4Kibanov@users.noreply.github.com>
2023-08-01 23:06:43 +0500

    №11

---
+++ AKibanov_11.1.py
@@ -0,0 +1,14 @@
+﻿#Нахождение факториала рекурсией
+def fac(n):
+    if n == 1:
+        return 1
+    return fac(n - 1) * n
+
+def Factorial_list(x):
+    list = []
+    n = fac(x)
+    for i in range(n, 0, -1):
+        list.append(fac(i))
+    print(f'Список из факториалов получившегося числа {n}: ', list)
+Factorial_list(int(input("Введите число, чтобы получить его факториал (например, 3): ")))
+input('Нажмите Enter для выхода из программы.')
