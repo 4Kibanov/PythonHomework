e9498eb958d1be9660ebec462882abd4e921b06e
4Kibanov <121761148+4Kibanov@users.noreply.github.com>
2023-08-01 19:08:02 +0500

    №10

---
+++ AKibanov_10.2.py
@@ -0,0 +1,11 @@
+﻿x, y = list(map(int, input('Введите через пробел диапазон двух чисел (например, 10 -5): ').split()))
+my_dict = {}
+s1, s2 = x, y
+step = s2 - s1
+if step < 0: step = -1
+else: step = 1
+
+for i in range(s1, s2 + step, step):
+    my_dict[i] = i ** i
+print(my_dict)
+input('Нажмите Enter для выхода из программы.')
