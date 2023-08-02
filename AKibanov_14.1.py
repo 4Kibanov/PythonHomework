a1374690e9599aa6a1cb04dc38fad6fe9c6f9a33
4Kibanov <121761148+4Kibanov@users.noreply.github.com>
2023-08-02 22:02:37 +0500

    №14

---
+++ AKibanov_14.1.py
@@ -0,0 +1,11 @@
+﻿my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+def a(list, n = 0):
+	print(list[n])
+	if n + 1 < len(list):
+		a(list, n + 1)
+	else: print('Конец списка')
+	return a
+
+a(my_list)
+input('Нажмите Enter для выхода из программы.')
